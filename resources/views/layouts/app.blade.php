9ba5ccf4466ef85b8d0c0a5b5f68a6e0d4755f0a
Torres Cesia <torrescesiaj@gmail.com>
2026-04-06 11:55:52 -0300

    Interfaz navbar: alineacion de iconos, mejora de los menu desplegables y creacion de clases de estilos en estilos-navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,6 @@
 
         {{-- Llamas a footer --}}
         @include('partials.footer')
-
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     </body>
 </html>
--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         {{-- Seccion logo + titulo --}}
         <a class="navbar-brand" href="{{ route('inicio') }}">
-            <span class="title-brand">🧉Matiensos</span>
+            <span class="title-nav">🧉Matiensos</span>
         </a>
         {{-- Adapta las opciones que siguen a pantalla de celulares --}}
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent">
@@ -11,14 +11,16 @@
         <div class="collapse navbar-collapse" id="navbarContent">
             
             {{-- Seccion de opciones centrales 'Productos', 'Nosotros' y 'Contacto' --}}
-            <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mx-auto opc-nav">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
                         Productos
                     </a>
                     <ul class="dropdown-menu">
                         <li><a class="dropdown-item" href="#">Mates</a></li>
+                        <li><a class="dropdown-item" href="#">Termos</a></li>
                         <li><a class="dropdown-item" href="#">Bombillas</a></li>
+                        <li><a class="dropdown-item" href="#">Bolsos</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="#">Ver todos</a></li>
                     </ul>
@@ -31,25 +33,27 @@
                 </li>
             </ul>
 
-            {{-- Seccion de botones 'Iniciar Sesion' y 'Registrarse' --}}
-            <div class="d-flex align-items-center ms-auto">
-                <div class="me-3">
-                    <a href="#" class="btn btn-sm btn-outline-dark me-2">Iniciar Sesión</a>
-                    <a href="#" class="btn btn-sm btn-dark">Registrarse</a>
+            {{-- Seccion de opciones 'Usuario', 'Favoritos' y 'Carrito de compras' --}}
+            <div class="icon-nav-config">
+                {{-- ícono menu de Usuario. Opciones 'Iniciar Sesion' y 'Registrarse' --}}
+                <div class="dropdown">
+                    <a class="icon-nav-btn dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                        <i class="bi bi-person-circle"></i>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Iniciar Sesión</a></li>
+                        <li><a class="dropdown-item" href="#">Registrarse</a></li>
+                    </ul>
                 </div>
-            </div>
-
-            {{-- Seccion de iconos de compras y favoritos --}}
-            <div class="d-flex gap-3">
-                <a href="#" class="text-dark">
-                    <i class="bi bi-heart fs-5"></i>
+                {{-- Seccion de iconos de compras y favoritos --}}
+                <a href="#" class="icon-nav-btn">
+                    <i class="bi bi-heart"></i>
                 </a>
-                <a href="#" class="text-dark position-relative">
-                    <i class="bi bi-bag-fill fs-5"></i>
-                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger" style="font-size: 0.6rem;">0</span>
+                <a href="#" class="icon-nav-btn">
+                    <i class="bi bi-bag-fill"></i>
+                    <span class="badge-notif">0</span>
                 </a>
             </div>
-
         </div>
     </div>
 </nav>
