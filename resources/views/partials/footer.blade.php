aae08334418a3c944aae3bba079c91fd46472ce7
Torres Cesia <torrescesiaj@gmail.com>
2026-04-26 15:50:50 -0300

    Link a wpp

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -20,7 +20,7 @@
             <div class="col-md-2 col-sm-6 mb-4 small">
                 <h5>Contactanos</h5>
                 <ul class="list-unstyled mb-3">
-                    <li><a href="https://wa.me/543644675174" target="_blank" class="text-decoration-none text-light opacity-75"><i class="bi bi-whatsapp me-2"></i>543644675174</a></li>
+                    <li><a href="https://wa.me/3782456372" target="_blank" class="text-decoration-none text-light opacity-75"><i class="bi bi-whatsapp me-2"></i>3782456372</a></li>
                     <li><a href="{{route('pagina-en-construccion')}}" class="text-decoration-none text-light opacity-75"><i class="bi bi-envelope me-2"></i>matiensos@gmail.com</a></li>
                     <li><a href="{{route('pagina-en-construccion')}}" class="text-decoration-none text-light opacity-75"><i class="bi bi-geo-alt me-2"></i>Av. Las Heras 727, Corrientes</a></li>
                 </ul>
