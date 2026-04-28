86e135e2d7fbe757c017da949c51167ca0537338
Luana Romero <luanaromero588@gmail.com>
2026-04-27 23:49:29 -0300

    arregle linkeo navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -29,9 +29,9 @@
                         Productos
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Mates</a></li>
-                        <li><a class="dropdown-item" href="#">Bombillas</a></li>
-                        <li><a class="dropdown-item" href="#">Termos</a></li>
+                        <li><a class="dropdown-item" href="{{ route('productos') }}#mates">Mates</a></li>
+                        <li><a class="dropdown-item" href="{{ route('productos') }}#bombillas">Bombillas</a></li>
+                        <li><a class="dropdown-item" href="{{ route('productos') }}#termos">Termos</a></li>
                         <li><hr class="dropdown-divider"></li>
                         <li><a class="dropdown-item" href="{{route('productos')}}">Ver todos</a></li>
                     </ul>
