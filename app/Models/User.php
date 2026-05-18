f0fe3111323b15962d9b05725ed7eccdbe7a2834
Torres Cesia <torrescesiaj@gmail.com>
2026-05-17 23:09:45 -0300

    Modelo User modificado. AuthController creado e iniciado

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,31 +2,45 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
+#[Fillable([    //Campos que se pueden cargar masivamente.
+    'name',
+    'last_name',
+    'email',
+    'password',
+    'role'
+])]
+
+#[Hidden([  //Campos que permanecen ocultos
+    'password',
+    'remember_token'
+])]
+
 class User extends Authenticatable
 {
-    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    
+    //Conversion de tipos de datos
     protected function casts(): array
     {
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+
+    public function isCustomer(): bool
+    {
+        return $this->role === 'customer';
     }
 }
