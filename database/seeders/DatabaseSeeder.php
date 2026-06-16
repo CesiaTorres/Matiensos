0e54f02d4bd55d1c2005c0f3fe1972c992d2f621
Luana Romero <luanaromero588@gmail.com>
2026-06-16 08:26:43 -0300

    añadi el bannerSeeder al DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,8 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             ProductSeeder::class,
             OrderSeeder::class,
-            ContactSeeder::class
+            ContactSeeder::class,
+            BannerSeeder::class,
         ]);
     }
 }
